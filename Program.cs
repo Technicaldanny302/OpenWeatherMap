4fe2c6385267a6de409aec47aae21dbd7b1a82c8
Dan Marino <technicaldanny302@gmail.com>
2024-09-16 19:11:33 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Nodes;
+using static System.Net.WebRequestMethods;
+
+namespace OpenWeatherMap
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var key = "cb6ec932ee52a1172b1432786c0245d4\r\n";
+            var city = "Birmingham";
+
+            var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
+            var response = client.GetAsync(weatherURL).Result;
+
+            JsonObject formattedResponse = JsonObject.Parse(response);
+
+        }
+    }
+}
